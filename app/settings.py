d594cc2a4dd7477e5787efd30a7cb619a0809632
d9magai <kumagai@sencorp.co.jp>
2019-05-31 13:58:42 +0000

    use mysql

--- app/settings.py
+++ app/settings.py
@@ -75,8 +75,16 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'database',
+        'USER': 'user',
+        'PASSWORD': 'secret',
+        'HOST': 'db',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+        'ATOMIC_REQUESTS': True,
     }
 }
 
