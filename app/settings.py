8553e734538d8102fc1b439a8abd276561563fcf
d9magai <kumagai@sencorp.co.jp>
2019-05-31 12:23:52 +0000

    allowed all hosts

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&omr01uygm=zm81+vyn3gjgd*2cxn=oh!!_ce8)**-ul=_s+z8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
